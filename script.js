ef1994e4689de88dcd21e45faf59766f7f808c79
Sri Raksha K.N. <131985128+SriRaksha-code@users.noreply.github.com>
2025-08-26 10:37:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("navLinks");
-
-hamburger.addEventListener("click", () => {
+function toggleMenu() {
+  const navLinks = document.getElementById("navLinks");
   navLinks.classList.toggle("show");
-});
+}
